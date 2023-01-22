a6ac8171c919a8cf586d54065155febb8a330555
AntoineA67 <adamy.antoine@gmail.com>
2023-01-22 02:17:27 +0100

    started game

---
+++ src/Game.tsx
@@ -0,0 +1,195 @@
+import { gsap } from "gsap";
+import { Draggable } from "gsap/Draggable";
+import {
+	Button,
+} from "@mui/material";
+import $ from 'jquery';
+import { dragContainerStyle, dropContainerStyle, dropZoneStyle, draggableStyle } from "./styles/GameStyles";
+
+gsap.registerPlugin(Draggable);
+
+var dropContainer = $(".drop-container");
+var dragContainer = $(".drag-container");
+
+var bases = [
+	$("#forward"),
+	$("#left"),
+	$("#right"),
+	$("#f1"),
+]
+
+var dropMargin = 5;
+var dropBorder = 3;
+var dropZones: any[] = [];
+var threshold = "50%"
+
+createDropZones(10);
+
+for (let i = 0; i < bases.length; i++) {
+	createDraggable(i);
+}
+
+// var checkButton = document.querySelector("#check-button");
+// checkButton.addEventListener("click", checkTiles);
+
+function checkTiles() {
+	for (var i = 0; i < dropZones.length; i++) {
+	  var tile = dropZones[i];
+	  
+	  if (!tile.child) {
+		continue;
+	  }
+	  console.log(tile.child.dataset.value)    
+	}
+  }
+
+function createDropZones(count: number) {
+	
+	for (var i = 0; i < count; i++) {
+		var dropZone:any = $(`<div class="drop-zone" data-value="${i}"/>`).appendTo(dropContainer);
+		dropZones.push(dropZone);
+		dropZone.child = null
+	}
+}
+
+function createDraggable(index: number) {
+
+	gsap.set(bases[index], {
+		x: 0,
+		y: index * 206,
+	})
+	
+	console.log("created")
+	var dragTarget = bases[index].clone().appendTo(dragContainer);
+	
+	Draggable.create(dragTarget, {
+		autoScroll: 1,
+		onDragEnd: checkDropZones
+	});
+	
+	function checkDropZonesOnlyMove(this: any) {
+		var insideDropContainer = this.hitTest(dropContainer, threshold);
+		
+		if (!insideDropContainer) {
+			dropZones[this.target.drop].child = null
+			this.kill();
+			this.target.remove();
+			snapBack(dragTarget, index);
+			return;
+		}
+
+		var dropZone = null;
+		var numZones = dropZones.length;
+		var i
+		for (i = 0; i < numZones; i++) {
+			
+			// Breaks on first hit...
+			if (this.hitTest(dropZones[i], threshold)) {
+				dropZone = dropZones[i];
+				break;
+			}
+		}
+
+		if (!dropZone) {
+			snapBack(dragTarget, index);
+		} else {
+			dropZones[this.target.drop].child = null
+			this.target.drop = i
+			if (dropZone.child) {
+				dropZone.child.remove()
+			}
+			dropZone.child = this.target
+			dropDraggable(dragTarget, dropZone);
+		}
+	}
+
+	function checkDropZones(this: any) {
+		this.target.drop = -1
+		var insideDropContainer = this.hitTest(dropContainer);
+		
+		if (!insideDropContainer) {
+			snapBack(dragTarget, index);
+			return;
+		}
+		
+		var dropZone = null;
+		var numZones = dropZones.length;
+		var i
+		for (i = 0; i < numZones; i++) {
+			
+			// Breaks on first hit...
+			if (this.hitTest(dropZones[i], threshold)) {
+				dropZone = dropZones[i];
+				break;
+			}
+		}
+		
+		if (!dropZone) {
+			snapBack(dragTarget, index);
+		} else {
+			this.kill();
+			Draggable.create(dragTarget, {
+				autoScroll: 1,
+				onDragEnd: checkDropZonesOnlyMove
+			});
+			createDraggable(index);
+			this.target.drop = i
+			if (dropZone.child) {
+				dropZone.child.remove()
+			}
+			dropZone.child = this.target
+			dropDraggable(dragTarget, dropZone);
+		}    
+	}
+}
+
+function dropDraggable(target: any, dropZone: { position: () => any; }) {
+	
+	var containerPos: any = dropContainer.offset();
+	var currentPos = target.offset();
+	var dropPos = dropZone.position();
+		
+	dropContainer.append(target);
+		
+	gsap.set(target, {
+		x: currentPos.left - containerPos.left - dropBorder,
+		y: currentPos.top - containerPos.top - dropBorder,
+	})
+	
+	gsap.to(target, {
+	duration: 0.1,
+		x: dropPos.left + dropMargin,
+		y: dropPos.top + dropMargin
+	});
+}
+
+function snapBack(target: gsap.TweenTarget, index: number) {
+	gsap.to(target, { duration: 0.2, x: 0, y: index * 206 });
+}
+
+export default function Game() {
+	return (
+		<div className="Game">
+
+			<Button size="large"
+					variant="contained"
+					onClick={checkTiles}>
+						Start
+			</Button>
+
+			<div style={{display: "flex", flexDirection: "row", justifyContent: "center", boxSizing: "border-box"}}>
+
+				<div className="drag-container" style={dragContainerStyle}>
+
+					<div className="draggable" style={draggableStyle} id="forward" data-value="forward">Forward</div>
+					<div className="draggable" style={draggableStyle} id="left" data-value="left">Left</div>
+					<div className="draggable" style={draggableStyle} id="right" data-value="right">Right</div>
+					<div className="draggable" style={draggableStyle} id="f1" data-value="f1">F1</div>
+
+				</div>
+				
+				<div className="drop-container" style={dropContainerStyle}></div>
+			</div>
+   		</div>
+	);
+}
---
+++ src/styles/GameStyles.tsx
@@ -0,0 +1,36 @@
+export const dragContainerStyle:React.CSSProperties = {
+	position: "absolute",
+	// display: "flex",
+	top: "100px",
+	left: "100px",
+	height: "206px",
+	width: "206px",
+	border: "3px solid #000",
+}
+
+export const dropContainerStyle:React.CSSProperties = {
+	position: "absolute",
+	top: "100px",
+	left: "500px",
+	width: "216px",
+	border: "3px solid #000",
+}
+
+export const dropZoneStyle:React.CSSProperties = {
+	margin: "5px",
+	border: "1px dashed #aaa",
+	height: "200px",
+	width: "200px",
+}
+
+export const draggableStyle:React.CSSProperties = {
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+	position: "absolute",
+	width: "200px",
+	height: "200px",
+	background: "dodgerblue",
+	top: "0",
+	left: "0",
+}
