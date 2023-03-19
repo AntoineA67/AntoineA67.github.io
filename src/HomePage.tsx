5fb5688e53e308b112a8a6f3b398e2f1f396fab4
AntoineA67 <adamy.antoine@gmail.com>
2023-03-19 22:10:04 +0100

    parallax

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -15,6 +15,7 @@ import Element from "./components/Element";
 import { NavigationBar } from "./components/NavigationBar";
 import { Contact } from "./Contact";
 import { SubscribePlusGameLink } from "./components/SubscribePlusGameLink";
+import { Parallax } from "react-parallax";
 
 const theme = createTheme({
 	palette: {
@@ -47,60 +48,63 @@ function HomePage() {
 		<ThemeProvider theme={theme}>
 			<div className="App">
 				<NavigationBar />
-				<Box component="div"
-					sx={{
-						backgroundImage: `url(${Background})`,
-						backgroundSize: "cover",
-						paddingTop: "60px",
-						paddingBottom: "60px",
-					}}
-				>
-					<CustomWrapper anchor="home">
-						<Element
-							buttonColor="#2C5EAF"
-							reverse={false}
-							image={WatiCar}
-							title="Apprendre le code sans écran ? C'est possible avec Sequency !"
-							description="Notre mission est claire : apporter à l'enfant toutes les clés de réussite dans un monde ultra-connecté."
-						/>
-					</CustomWrapper>
-			
-					<CustomWrapper anchor="test">
-						<Element
-							buttonColor="#2C5EAF"
-							reverse={true}
-							description="les enfants peuvent manipuler les blocs de programmation avec leurs mains et les explorer sous tous les angles, ce qui rend l'apprentissage de la logique informatique encore plus concret et amusant."
-							title="Avec Sequency, la programmation devient un jeu d'enfant grâce à des blocs connectables et intuitifs."
-							video={LosBlockos} />
-					</CustomWrapper>
-					<CustomWrapper anchor="test">
-						<Element
-							buttonColor="#2C5EAF"
-							reverse={false}
-							description="En connectant les blocs entre eux, les enfants peuvent créer des séquences de programmation personnalisées et voir leur robot réaliser chaque commande dans l'ordre. Cela développe leur créativité et leur capacité à résoudre des problèmes de manière logique."
-							title="Créez des programmes en connectant des blocs ! 🤖💡"
-							video={LosBlockosConnectos} />
-					</CustomWrapper>
-					<CustomWrapper anchor="test">
-						<Element
-							buttonColor="#2C5EAF"
-							reverse={true}
-							description="Une fois le programme prêt, il suffit de le lancer et de voir le robot se mettre en mouvement ! Les enfants peuvent suivre leur robot et s'amuser à observer les résultats de leur travail en temps réel, ce qui les encourage à continuer, à apprendre et à explorer."						
-							title="Laissez votre enfant voir concrètement les résultats de son travail avec Sequency !"
-							video={LETSGOOOBLOCKOS} />
-					</CustomWrapper>
+				<Parallax bgImage={Background} strength={2000} blur={{ min: -5, max: 5 }}>
+					<Box component="div"
+						sx={{
+							// backgroundImage: `url(${Background})`,
+							// backgroundSize: "cover",
+							paddingTop: "60px",
+							paddingBottom: "60px",
+						}}
+					>
+						<CustomWrapper anchor="home">
+							<Element
+								buttonColor="#2C5EAF"
+								reverse={false}
+								image={WatiCar}
+								title="Apprendre le code sans écran ? C'est possible avec Sequency !"
+								description="Notre mission est claire : apporter à l'enfant toutes les clés de réussite dans un monde ultra-connecté."
+							/>
+						</CustomWrapper>
 
-					<CustomWrapper anchor="subscribe">
-						<SubscribePlusGameLink/>
-					</CustomWrapper>
+						<CustomWrapper anchor="test">
+							<Element
+								buttonColor="#2C5EAF"
+								reverse={true}
+								description="les enfants peuvent manipuler les blocs de programmation avec leurs mains et les explorer sous tous les angles, ce qui rend l'apprentissage de la logique informatique encore plus concret et amusant."
+								title="Avec Sequency, la programmation devient un jeu d'enfant grâce à des blocs connectables et intuitifs."
+								video={LosBlockos} />
+						</CustomWrapper>
+						<CustomWrapper anchor="test">
+							<Element
+								buttonColor="#2C5EAF"
+								reverse={false}
+								description="En connectant les blocs entre eux, les enfants peuvent créer des séquences de programmation personnalisées et voir leur robot réaliser chaque commande dans l'ordre. Cela développe leur créativité et leur capacité à résoudre des problèmes de manière logique."
+								title="Créez des programmes en connectant des blocs ! 🤖💡"
+								video={LosBlockosConnectos} />
+						</CustomWrapper>
+						<CustomWrapper anchor="test">
+							<Element
+								buttonColor="#2C5EAF"
+								reverse={true}
+								description="Une fois le programme prêt, il suffit de le lancer et de voir le robot se mettre en mouvement ! Les enfants peuvent suivre leur robot et s'amuser à observer les résultats de leur travail en temps réel, ce qui les encourage à continuer, à apprendre et à explorer."
+								title="Laissez votre enfant voir concrètement les résultats de son travail avec Sequency !"
+								video={LETSGOOOBLOCKOS} />
+						</CustomWrapper>
 
-					<CustomWrapper anchor="team">
-						<Team />
-					</CustomWrapper>
-					<CustomWrapper anchor="contact">
-						<Contact />
-					</CustomWrapper>
-				</Box>
+						<CustomWrapper anchor="subscribe">
+							<SubscribePlusGameLink />
+						</CustomWrapper>
+
+						<CustomWrapper anchor="team">
+							<Team />
+						</CustomWrapper>
+						<CustomWrapper anchor="contact">
+							<Contact />
+						</CustomWrapper>
+					</Box>
+				</Parallax>
+
 			</div >
 		</ThemeProvider >
 	)
