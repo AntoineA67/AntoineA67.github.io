91e938ef1b91ec4f6a6c276d339e5e7c6b99f5e1
Antoine Adamy <antoine.adamy@gmail.com>
2023-09-27 12:13:30 +0200

    fix

--- src/components/Game3D.jsx
+++ src/components/Game3D.jsx
@@ -12,7 +12,7 @@ import { StartTextModel } from './models/StartTextModel'
 import { Box, Typography, Backdrop, CircularProgress } from '@mui/material'
 import { suspend } from 'suspend-react'
 
-const sunset = import('@pmndrs/assets/hdri/sunset.exr').then((module) => module.default)
+const dawn = import('@pmndrs/assets/hdri/dawn.exr').then((module) => module.default)
 
 const canvasStyle = {
 	width: "100vw",
@@ -313,9 +313,7 @@ export default function Game3D() {
 					{/* <Cloud scale={.1} position={[-1, 2, -3]} />
 					<Cloud scale={.1} position={[2, 1.5, 2]} /> */}
 					<ContactShadows position={[.2, -1.5, 0.2]} opacity={0.75} scale={10} blur={2.5} far={3} />
-					{/* <Environment files="./venice_sunset_1k.hdr" /> */}
-					{/* <Environment preset="forest" /> */}
-					<Environment files={suspend(sunset)} />
+					<Environment files={suspend(dawn)} />
 				</Canvas>
 			</Suspense>
 			<div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
